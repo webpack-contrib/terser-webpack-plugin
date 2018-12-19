8cd50d787108c58dbcc3ea9d520186960604176a
evilebottnawi <sheo13666q@gmail.com>
2018-12-19 17:19:09 +0300

    fix: regenerate `contenthash`

--- test/TerserPlugin.test.js
+++ test/TerserPlugin.test.js
@@ -42,6 +42,40 @@ describe('TerserPlugin', () => {
     });
   });
 
+  it.only('should works regenerate hash', () => {
+    const compiler = createCompiler({
+      entry: {
+        js: `${__dirname}/fixtures/entry.js`,
+        mjs: `${__dirname}/fixtures/entry.mjs`,
+        importExport: `${__dirname}/fixtures/import-export/entry.js`,
+        AsyncImportExport: `${__dirname}/fixtures/async-import-export/entry.js`,
+      },
+      output: {
+        path: `${__dirname}/dist`,
+        filename: '[name].[contenthash].js',
+        chunkFilename: '[id].[name].[contenthash].js',
+      },
+    });
+
+    new TerserPlugin().apply(compiler);
+
+    return compile(compiler).then((stats) => {
+      const errors = stats.compilation.errors.map(cleanErrorStack);
+      const warnings = stats.compilation.warnings.map(cleanErrorStack);
+
+      expect(errors).toMatchSnapshot('errors');
+      expect(warnings).toMatchSnapshot('warnings');
+
+      for (const file in stats.compilation.assets) {
+        if (
+          Object.prototype.hasOwnProperty.call(stats.compilation.assets, file)
+        ) {
+          expect(stats.compilation.assets[file].source()).toMatchSnapshot(file);
+        }
+      }
+    });
+  });
+
   it('isSourceMap method', () => {
     expect(TerserPlugin.isSourceMap(null)).toBe(false);
     expect(TerserPlugin.isSourceMap()).toBe(false);
