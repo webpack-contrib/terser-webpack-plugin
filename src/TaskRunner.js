bb506094ce25d78071430162a634fe35dd5da3ac
evilebottnawi <sheo13666q@gmail.com>
2019-05-14 14:20:04 +0300

    fix: fallback for cache directory

--- src/TaskRunner.js
+++ src/TaskRunner.js
@@ -13,7 +13,9 @@ export default class TaskRunner {
   constructor(options = {}) {
     const { cache, parallel } = options;
     this.cacheDir =
-      cache === true ? findCacheDir({ name: 'terser-webpack-plugin' }) : cache;
+      cache === true
+        ? findCacheDir({ name: 'terser-webpack-plugin' }) || os.tmpdir()
+        : cache;
     // In some cases cpus() returns undefined
     // https://github.com/nodejs/node/issues/19022
     const cpus = os.cpus() || { length: 1 };
