5768695c69305f6a5d01da142ad7e4cdb809f1b7
evilebottnawi <sheo13666q@gmail.com>
2019-09-12 19:51:37 +0300

    feat: improve naming of comments extract file

--- test/extractComments-option.test.js
+++ test/extractComments-option.test.js
@@ -152,13 +152,18 @@ describe('extractComments option', () => {
   });
 
   it('should match snapshot when extracts comments to multiple files', async () => {
+    expect.assertions(13);
+
     new TerserPlugin({
       extractComments: {
         condition: true,
-        filename(file) {
+        filename: (file, fileData) => {
+          expect(file).toBeDefined();
+          expect(fileData).toBeDefined();
+
           return file.replace(/(\.\w+)$/, '.license$1');
         },
-        banner(licenseFile) {
+        banner: (licenseFile) => {
           return `License information can be found in ${licenseFile}`;
         },
       },
@@ -237,6 +242,8 @@ describe('extractComments option', () => {
   });
 
   it('should match snapshot when no condition, extracts only `/@license/i` comments', async () => {
+    expect.assertions(13);
+
     new TerserPlugin({
       terserOptions: {
         output: {
@@ -244,10 +251,13 @@ describe('extractComments option', () => {
         },
       },
       extractComments: {
-        filename(file) {
+        filename: (file, fileData) => {
+          expect(file).toBeDefined();
+          expect(fileData).toBeDefined();
+
           return file.replace(/(\.\w+)$/, '.license$1');
         },
-        banner(licenseFile) {
+        banner: (licenseFile) => {
           return `License information can be found in ${licenseFile}`;
         },
       },
@@ -283,6 +293,106 @@ describe('extractComments option', () => {
         filename: 'extracted-comments.js',
         banner(licenseFile) {
           return `License information can be found in ${licenseFile}`;
+        },
+      },
+    }).apply(compiler);
+
+    const stats = await compile(compiler);
+
+    const errors = stats.compilation.errors.map(cleanErrorStack);
+    const warnings = stats.compilation.warnings.map(cleanErrorStack);
+
+    expect(errors).toMatchSnapshot('errors');
+    expect(warnings).toMatchSnapshot('warnings');
+    expect(getAssets(stats, compiler)).toMatchSnapshot('assets');
+  });
+
+  it('should match snapshot when extracts comments to files with query string', async () => {
+    compiler = createCompiler({
+      entry: {
+        one: `${__dirname}/fixtures/comments.js`,
+        two: `${__dirname}/fixtures/comments-2.js`,
+        three: `${__dirname}/fixtures/comments-3.js`,
+        four: `${__dirname}/fixtures/comments-4.js`,
+      },
+      output: {
+        filename: 'filename/[name].js?[chunkhash]',
+        chunkFilename: 'chunks/[id].[name].js?[chunkhash]',
+      },
+    });
+
+    new TerserPlugin().apply(compiler);
+
+    const stats = await compile(compiler);
+
+    const errors = stats.compilation.errors.map(cleanErrorStack);
+    const warnings = stats.compilation.warnings.map(cleanErrorStack);
+
+    expect(errors).toMatchSnapshot('errors');
+    expect(warnings).toMatchSnapshot('warnings');
+    expect(getAssets(stats, compiler)).toMatchSnapshot('assets');
+  });
+
+  it('should match snapshot when extracts comments to files with query string and with placeholders', async () => {
+    compiler = createCompiler({
+      entry: {
+        one: `${__dirname}/fixtures/comments.js`,
+        two: `${__dirname}/fixtures/comments-2.js`,
+        three: `${__dirname}/fixtures/comments-3.js`,
+        four: `${__dirname}/fixtures/comments-4.js`,
+      },
+      output: {
+        filename: 'filename/[name].js?[chunkhash]',
+        chunkFilename: 'chunks/[id].[name].js?[chunkhash]',
+      },
+    });
+
+    new TerserPlugin({
+      extractComments: {
+        condition: true,
+        filename: '[file].LICENSE?query=[query]&filebase=[filebase]',
+        banner(licenseFile) {
+          return `License information can be found in ${licenseFile}`;
+        },
+      },
+    }).apply(compiler);
+
+    const stats = await compile(compiler);
+
+    const errors = stats.compilation.errors.map(cleanErrorStack);
+    const warnings = stats.compilation.warnings.map(cleanErrorStack);
+
+    expect(errors).toMatchSnapshot('errors');
+    expect(warnings).toMatchSnapshot('warnings');
+    expect(getAssets(stats, compiler)).toMatchSnapshot('assets');
+  });
+
+  it('should match snapshot when extracts comments to files with query string and when filename is a function', async () => {
+    expect.assertions(13);
+
+    compiler = createCompiler({
+      entry: {
+        one: `${__dirname}/fixtures/comments.js`,
+        two: `${__dirname}/fixtures/comments-2.js`,
+        three: `${__dirname}/fixtures/comments-3.js`,
+        four: `${__dirname}/fixtures/comments-4.js`,
+      },
+      output: {
+        filename: 'filename/[name].js?[chunkhash]',
+        chunkFilename: 'chunks/[id].[name].js?[chunkhash]',
+      },
+    });
+
+    new TerserPlugin({
+      extractComments: {
+        condition: true,
+        filename: (file, fileData) => {
+          expect(file).toBeDefined();
+          expect(fileData).toBeDefined();
+
+          // A file can contain a query string (for example when you have `output.filename: '[name].js?[chunkhash]'`)
+          // You must consider this
+          return file.replace(/\.(\w+)($|\?)/, '.$1.LICENSE$2');
         },
       },
     }).apply(compiler);
