fcc3d5be12b7bedc2c1ab2dd4fe8357884e2ba0e
evilebottnawi <sheo13666q@gmail.com>
2020-01-27 19:27:08 +0300

    fix: reduce memory usage in parallel mode

--- test/helpers/getErrors.js
+++ test/helpers/getErrors.js
@@ -1,5 +1,5 @@
 import normalizeErrors from './normalizeErrors';
 
 export default (stats) => {
-  return normalizeErrors(stats.compilation.errors.sort());
+  return normalizeErrors(stats.compilation.errors).sort();
 };
